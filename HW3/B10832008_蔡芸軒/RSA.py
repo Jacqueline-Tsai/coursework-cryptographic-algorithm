c14f49fc415232c7620f872b199494f6d4e8b21c
Jacqueline-Tsai <jaynnah01180118@gmail.com>
2022-05-09 20:10:27 +0800

    add report and test

--- HW3/B10832008_蔡芸軒/RSA.py
+++ HW3/B10832008_蔡芸軒/RSA.py
@@ -3,38 +3,34 @@ number_of_bits, min_range_of_e = 1024, pow(2, 50) #1024, 2**50
 
 def pow_by_square_and_multiply(x, n, p):
     # return x^n % p
-    binN = bin(n)[3:]
-    result = x
-    for i in binN:
-        result = (result * result) % p
+    x, b, ans = x % p, bin(n)[3:], x
+    for i in b:
+        ans = (ans * ans) % p
         if i == '1':
-            result = (result * x) % p
-    return result
+            ans = (ans * x) % p
+    return ans
 
-def miller_rabin_test(mrc):
-	maxDivisionsByTwo = 0
-	ec = mrc-1
-	while ec % 2 == 0:
-		ec >>= 1
-		maxDivisionsByTwo += 1
-	assert(2**maxDivisionsByTwo * ec == mrc-1)
+def miller_rabin_test(n):
+	s, d = 0, n - 1
+	while d % 2 == 0:
+		d >>= 1
+		s += 1
 
-	def trialComposite(round_tester):
-		if pow_by_square_and_multiply(round_tester, ec, mrc) == 1:
+	def trial_composite(a):
+		if pow_by_square_and_multiply(a, d, n) == 1:
 			return False
-		for i in range(maxDivisionsByTwo):
-			if pow_by_square_and_multiply(round_tester, 2**i * ec, mrc) == mrc-1:
+		for i in range(s):
+			if pow_by_square_and_multiply(a, 2**i * d, n) == n-1:
 				return False
 		return True
 
-	numberOfRabinTrials = 20
-	for i in range(numberOfRabinTrials):
-		round_tester = random.randrange(2, mrc)
-		if trialComposite(round_tester):
+	for i in range(20):
+		a = random.randrange(2, n)
+		if trial_composite(a):
 			return False
 	return True
 
-def generate_n_bit_number(n):
+def generate_n_bit_prime(n):
     first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293,307, 311, 313, 317, 331, 337, 347, 349]
     while True:
         ans = random.randrange(2**(n-1) + 1, 2**n - 1)
@@ -47,7 +43,7 @@ def generate_n_bit_number(n):
             return ans
 
 def key_generation(n_bit):
-    p, q = generate_n_bit_number(n_bit), generate_n_bit_number(n_bit)
+    p, q = generate_n_bit_prime(n_bit), generate_n_bit_prime(n_bit)
     N, r = p * q, (p - 1) * (q - 1)
     e = random.randrange(min_range_of_e, r)  # e can not be too small
     while math.gcd(e, r) != 1:
@@ -62,17 +58,18 @@ def RSA(msg, N, key):
 
 def RSA_by_chinese_remainder_theorem(msg, p, q, key):
     msg, p, q, key = int.from_bytes(base64.b64decode(msg), 'big'), max(int(p), int(q)), min(int(p), int(q)), int(key)
-    m1 = pow(msg, key % (p-1), p)
-    m2 = pow(msg, key % (q-1), q)
-    qInv = pow(q, -1, p)
-    h = (qInv*(m1 - m2)) % p
-    msg = m2 + h*q
-
+    m1 = pow_by_square_and_multiply(msg, key % (p-1), p)
+    m2 = pow_by_square_and_multiply(msg, key % (q-1), q)
+    q_inv = pow(q, -1, p)
+    h = (q_inv * (m1 - m2)) % p
+    msg = m2 + h * q
     return base64.b64encode(msg.to_bytes(math.ceil(math.log2(msg)/8), 'big')).decode()
 
 def test():
+    x, n, p = random.randrange(200, 500000000000000000), random.randrange(200, 500000000000000000), random.randrange(200, 500000000000000000)
+    if pow_by_square_and_multiply(x, n, p) != pow(x, n, p): print(0)
     p, q, N, r, e, d = key_generation(number_of_bits)
-    msg = generate_n_bit_number(number_of_bits)
+    msg = generate_n_bit_prime(number_of_bits)
     msg = base64.b64encode(msg.to_bytes(math.ceil(math.log2(msg)/8), 'big')).decode()
     if RSA(RSA(msg, N, e), N, d) != msg: print(1)
     if RSA(RSA(msg, N, d), N, e) != msg: print(2)
